038889d8238fc81133550c2f7dc6163ca089a209
mgreen <mgreen@bournemouth.ac.uk>
2026-02-04 18:46:14 +0000

    just python stuff

---
+++ trialmaker.py
@@ -0,0 +1,19 @@
+negative_pictures = ["A011.jpg", "A038.jpg", "A040.jpg", "A042.jpg", "A062.jpg", "A115.jpg", "A123.jpg", "A127.jpg", "I169.jpg", "OASIS_I166.jpg", "OASIS_I167.jpg", "OASIS_I22.jpg", "OASIS_I24.jpg", "OASIS_I273.jpg", "OASIS_I274.jpg", "OASIS_I281.jpg", "OASIS_I282.jpg", "Sn079.jpg", "Sp009.jpg", "Sp020.jpg", "Sp051.jpg", "Sp075.jpg", "Sp125.jpg", "Sp126.jpg"]
+
+neutral_pictures = ["N003.bmp", "N004.bmp", "N006.bmp", "N009.bmp", "N010.bmp", "N011.bmp", "N014.bmp", "N017.bmp", "N020.bmp", "N024.bmp", "N026.bmp", "N028.bmp", "N031.bmp", "N036.bmp", "N040.bmp", "N047.bmp", "N061.bmp", "N067.bmp", "N070.bmp", "N081.bmp", "N082.bmp", "N091.bmp", "N092.bmp", "N102.bmp"]
+
+positive_pictures = ["I132.jpg", "OASIS_I133.jpg", "OASIS_I134.jpg", "OASIS_I252.jpg", "OASIS_I254.jpg", "OASIS_I256.jpg", "P060.jpg", "P066.jpg", "P073.jpg", "P075.jpg", "P077.jpg", "P078.jpg", "P079.jpg", "P080.jpg", "P081.jpg", "P082.jpg", "P083.jpg", "P095.jpg", "P096.jpg", "P099.jpg", "P101.jpg", "P113.jpg", "P114.jpg", "P122.jpg"]
+
+video_filenames = ["BabyCooking.mp4", "BabyTeeth.mp4", "BabyTree.mp4", "Backflips.mp4", "Ballerina.mp4", "Ballett.mp4", "Basketball.mp4", "Beard.mp4", "Bicycle.mp4", "BicycleBoy.mp4", "Boysplaying.mp4", "Canoe.mp4", "CarDriving.mp4", "ChildHockey.mp4", "CityWalking.mp4", "CookingDadDaughter.mp4", "CookingMan.mp4", "CookingPumpkin.mp4", "CookingShow.mp4", "CookingShowII.mp4", "DancingGirl.mp4", "DancingGuy.mp4", "DiscusThrow.mp4", "DogWalking.mp4", "DrivingCar.mp4", "GiftWrapping.mp4", "GrandmaJumping.mp4", "GrassTrimming.mp4", "GrassTrimmingII.mp4", "GuySmoking.mp4", "GymnasticsChina.mp4", "HairMannequin.mp4", "HairStyling.mp4", "HairdresserDonation.mp4", "Hairrollers.mp4", "Harmonica.mp4", "HarmonicaStanding.mp4", "HighJump.mp4", "HorseRiding.mp4", "HorseTrail.mp4", "Interview.mp4", "KidswithDad.mp4", "MakeupArtist.mp4", "MakeupArtistGirl.mp4", "ManBikeRiding.mp4", "Motorbikes.mp4", "Nails.mp4", "PianoRecital.mp4", "PianoYanni.mp4", "Pipes.mp4", "PoolJumping.mp4", "PoolJumpingOutdoors.mp4", "RockClimbing.mp4", "RockClimbingII.mp4", "ShoeCleaning.mp4", "ShoeCleaningII.mp4", "SnowShovelling.mp4", "Spinning.mp4", "Tennis.mp4", "Vaccum.mp4", "WalkingDog.mp4", "WashingBedsheets.mp4", "WashingDishes.mp4", "WashingFace.mp4", "Weightlifing.mp4", "WhaleWatching.mp4", "WindowCleaner.mp4", "WindowCleanersCostumes.mp4", "WomanCooking.mp4", "WoodchoppingDog.mp4", "WoodchoppingSnow.mp4", "Woodpolishing.mp4"]
+
+video_set_a = video_filenames[0:24]
+video_set_b = video_filenames[24:48]
+video_set_c = video_filenames[48:72]
+
+print((video_set_a))
+print((video_set_b))
+print((video_set_c))
+
+print(len(video_set_a))
+print(len(video_set_b))
+print(len(video_set_c))
